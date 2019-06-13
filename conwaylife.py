9f930d61a0fb3d1235e7d43c01f2fc83541aa232
nengjinhe <nengjinhe@outlook.com>
2019-06-13 11:20:13 +0800

    add first draft

--- conwaylife.py
+++ conwaylife.py
@@ -8,13 +8,28 @@ class ConwayGame():
         self.T = T
         self.old_grid = numpy.zeros(N * N, dtype = 'int').reshape(N, N)
         self.new_grid = numpy.zeros(N * N, dtype = 'int').reshape(N, N)
-
-        for i in range(0, self.N):
-            for j in range(0, self.N):
-                if (random.randint(0,100) < 5):
-                    self.old_grid[i][j] = 1
-                else:
-                    self.old_grid[i][j] = 0
+        if a == 1:
+            for i in range(0, self.N):
+                for j in range(0, self.N):
+                    if (random.randint(0,100) < 5):
+                        self.old_grid[i][j] = 1
+                    else:
+                        self.old_grid[i][j] = 0
+        elif a == 2:
+             for i in range(int(self.N / 2) - 5, int(self.N / 2) + 5):
+                 self.old_grid[int(self.N / 2)][i] = 1
+        elif a == 3:
+            self.old_grid[int(self.N / 2) - 1][int(self.N / 2) - 1] = 1
+            self.old_grid[int(self.N / 2) - 1][int(self.N / 2)] = 1
+            self.old_grid[int(self.N / 2) - 1][int(self.N / 2) + 1] = 1
+            self.old_grid[int(self.N / 2)][int(self.N / 2) + 1] = 1
+            self.old_grid[int(self.N / 2) + 1][int(self.N / 2)] = 1
+        elif a == 4:
+            for j in range(int(self.N / 2) - 2, int(self.N / 2) + 3):
+                self.old_grid[j][int(self.N / 2) - 2] = 1
+                self.old_grid[j][int(self.N / 2) + 2] = 1
+            self.old_grid[int(self.N / 2) - 2][int(self.N / 2)] = 1
+            self.old_grid[int(self.N / 2) + 2][int(self.N / 2)] = 1
 
     def live_neighbors(self, i, j):
         s = 0
@@ -43,13 +58,46 @@ class ConwayGame():
                         self.new_grid[i][j] = 0
                     elif (self.old_grid[i][j] == 0 and live == 3 ):
                         self.new_grid[i][j] = 1
-            time.sleep(0.1)
+            time.sleep(b)
             pylab.pcolormesh(self.new_grid)
             pylab.show()
-            pylab.close('all')
+            pylab.cla()
             self.old_grid = self.new_grid.copy()
             t += 1
 
 if __name__ == '__main__':
-    con = ConwayGame(N = 100,T =5)
+    print("##########################")
+    print("Select a Starting Pattern:")
+    print("  1: Random Grid")
+    print("  2: 10 cell Row")
+    print("  3: Glider")
+    print("  4: Exploder")
+    choice = input("Your Choice (1-4):")
+    a = 0
+    if choice == "1":
+        a = 1
+    elif choice == "2":
+        a = 2
+    elif choice == "3":
+        a = 3
+    elif choice == "4":
+        a = 4
+    else:
+        print("Invalid Choice!")
+    print("Select a speed:")
+    print("  1: Quick Speed")
+    print("  2: Normal Speed")
+    print("  3: Slow Speed")
+    choice = input("Your Choice (1-3):")
+    b = 0
+    if choice == "1":
+        b = 0.1
+    elif choice == "2":
+        b = 0.5
+    elif choice == "3":
+        b = 1
+    else:
+        print("Invalid Choice!")
+    con = ConwayGame(N=100, T=50)
     con.state_change()
+
